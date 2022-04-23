51d5e3d7541494c1bd6294717f0379baf6f66069
Jeremy Oden <jerem.oden@gmail.com>
2022-04-23 12:23:58 +0200

    Add utils for Adc.

--- src/Util/Cpp.hpp
+++ src/Util/Cpp.hpp
@@ -19,4 +19,13 @@ namespace Util
     template <class T>
     struct is_same<T, T> : integral_constant<bool, true> {};
 
+    template<bool B, class T = void>
+    struct enable_if {};
+    
+    template<class T>
+    struct enable_if<true, T> { using type = T; };
+
+    template< bool B, class T = void >
+    using enable_if_t = typename enable_if<B,T>::type;
+
 } // namespace Util
