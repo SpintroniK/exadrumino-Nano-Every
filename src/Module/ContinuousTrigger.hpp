f9498f57d2de786c52d588ae8e4eabf9ba5c749d
Jeremy Oden <jerem.oden@gmail.com>
2022-05-03 22:18:23 +0200

    Hi-hat controller (basic) test.

--- src/Module/ContinuousTrigger.hpp
+++ src/Module/ContinuousTrigger.hpp
@@ -17,9 +17,32 @@ namespace Module
 
         }
 
+        void Process(uint8_t value, uint8_t currentTime)
+        {
+            if(value > 200)
+            {
+                state = 1;
+            }
+            else
+            {
+                state = 0;
+            }
+        }
+
+        auto GetChannel() const noexcept
+        {
+            return adcChannel;
+        }
+
+        auto GetState() const noexcept
+        {
+            return state;
+        }
+
     private:
 
         ADC_MUXPOS_t adcChannel{};
+        uint8_t state{0}; // 0 => closed / 1 => open
 
     };
 
--- src/Module/Instruments/HiHat.hpp
+++ src/Module/Instruments/HiHat.hpp
@@ -11,23 +11,59 @@ namespace Module
     {
 
     public:
-        HiHat(Trigger&& t, ContinuousTrigger&& ct, uint8_t note)
-        : trigger{static_cast<Trigger&&>(t)}, controller{static_cast<ContinuousTrigger&&>(ct)} ,midiNote{note}
+        HiHat(Trigger&& t, ContinuousTrigger&& ct, uint8_t noteOpen, uint8_t noteClosed)
+        : trigger{static_cast<Trigger&&>(t)}, 
+        controller{static_cast<ContinuousTrigger&&>(ct)}, 
+        midiNoteOpen{noteOpen}, midiNoteClosed{noteClosed}
         {
 
         }
 
         ~HiHat() = default;
 
-        void Process(uint8_t value, uint8_t currentTime) noexcept
+        void ProcessTrigger(uint8_t value, uint8_t currentTime) noexcept
         {
+            trigger.Process(value, currentTime);
+        }
 
+        void ProcessController(uint8_t value, uint8_t currentTime) noexcept
+        {
+            controller.Process(value, currentTime);
+        }
+
+        auto GetMidiNote() const noexcept
+        {
+            return controller.GetState() == 0 ? midiNoteClosed : midiNoteOpen;
+        }
+
+
+        auto GetVelocity() const noexcept
+        {
+            return trigger.GetVelocity();
+        }
+
+
+        void Reset() noexcept
+        {
+            trigger.Reset();
+        }
+
+        auto GetTriggerChannel() const noexcept
+        {
+            return trigger.GetChannel();
+        }
+
+        auto GetControllerChannel() const noexcept
+        {
+            return controller.GetChannel();
         }
 
     private:
 
         Trigger trigger;
         ContinuousTrigger controller;
+        uint8_t midiNoteOpen;
+        uint8_t midiNoteClosed;
         uint8_t midiNote;
     };
     
